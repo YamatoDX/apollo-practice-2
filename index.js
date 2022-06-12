1b79b6702ade7e5ecb5f7f0460fbb7e0f49b23c4
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-06-12 22:11:42 +0600

    scractch 1 done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { allProducts, allOrgs } = require("./db");
 
 const typeDefs = gql`
     type Product{
@@ -9,6 +10,7 @@ const typeDefs = gql`
         country: String
         city: String
         productId: String
+        organisationId: String
         organisation: Organistion
     }
 
@@ -17,7 +19,6 @@ const typeDefs = gql`
         name: String
         city: String
         country: String
-        organistionId: String
         products: [Product]
     }
 
@@ -28,3 +29,49 @@ const typeDefs = gql`
         getOrganisationId: (id: String) => Organisation
     }
 `;
+
+const resolvers = {
+  Query: {
+    queryProduct: () => allProducts,
+    getProductById: (parent, args, ctx) => {
+      const argId = args.id;
+      return (
+        allProducts.filter((eachProduct) => eachProduct._id === argId)?.[0] ??
+        {}
+      );
+    },
+    queryOrganisation: () => allOrgs,
+    getOrganisationById: (parent, args, ctx) => {
+      const argId = args.id;
+      return allOrgs.filter((eachOrg) => eachOrg._id === argId)?.[0] ?? {};
+    },
+  },
+  Product: {
+    organisation: (parent, args, ctx) => {
+      const parentOrganisationId = parent.organisationId;
+      return (
+        allOrgs.filter(
+          (eachOrg) => eachOrg._id === parentOrganisationId
+        )?.[0] ?? {}
+      );
+    },
+  },
+  Organisation: {
+    products: (parent, args, ctx) => {
+      const parentId = parent._id;
+      return allProducts.filter(
+        (eachProduct) => eachProduct.organisationId === parentId
+      );
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+});
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`Listening to ${url}`);
+});
