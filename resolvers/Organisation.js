a13b7686ffcf6267ea12c50abfd58067aff4c423
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-06-13 08:54:57 +0600

    Organisation resolvers updated with context

--- resolvers/Organisation.js
+++ resolvers/Organisation.js
@@ -1,5 +1,6 @@
 const Organisation = {
   products: (parent, args, ctx) => {
+    const { allProducts } = ctx;
     const parentId = parent._id;
     return allProducts.filter(
       (eachProduct) => eachProduct.organisationId === parentId
