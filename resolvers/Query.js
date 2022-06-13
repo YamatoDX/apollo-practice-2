075d0c173363836c54a62f5355fa9fd0611110e2
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-06-13 08:54:06 +0600

    Query file updated with context

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,6 +1,7 @@
 const Query = {
   queryProduct: () => allProducts,
   getProductById: (parent, args, ctx) => {
+    const { allProducts } = ctx;
     const argId = args.id;
     return (
       allProducts.filter((eachProduct) => eachProduct._id === argId)?.[0] ?? {}
@@ -8,6 +9,7 @@ const Query = {
   },
   queryOrganisation: () => allOrgs,
   getOrganisationById: (parent, args, ctx) => {
+    const { allOrgs } = ctx;
     const argId = args.id;
     return allOrgs.filter((eachOrg) => eachOrg._id === argId)?.[0] ?? {};
   },
